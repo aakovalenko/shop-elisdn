592ad8bb491eb3ca943a16b7614a9513b3c67090
kaaSLDN <kaapl@gmail.com>
2017-10-10 17:57:26 +0300

    install admin lte

---
+++ common/bootstrap/SetUp.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andri
+ * Date: 10.10.17
+ * Time: 16:03
+ */
+
+namespace common\bootstrap;
+
+use yii\base\BootstrapInterface;
+
+class SetUp implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+
+    }
+}
